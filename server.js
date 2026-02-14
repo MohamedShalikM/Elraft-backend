12687bd6953414c018c054ab4b1d439376eb2131
MohamedShalikM <mohamedshalik25.it@gmail.com>
2026-02-14 13:06:25 +0530

    changes 1

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://elraft-fashion-ql9x6lfqo-mohamed-shaliks-projects.vercel.app",
+      "http://localhost:3000",
+    ],
+  }),
+);
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
@@ -23,7 +31,7 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/Elraft")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-  
+
 module.exports = app; 
 // app.listen(5000, () => {
 //   console.log("Server running on port 5000");
