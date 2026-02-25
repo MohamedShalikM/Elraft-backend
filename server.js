6f9b4be9674c24c908c996957d9828d74ef4fd2f
MohamedShalikM <mohamedshalik25.it@gmail.com>
2026-02-25 11:40:20 +0530

    cors update 3.0

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 
+require("dotenv").config();
 const app = express();
 
+const allowedOrigins = [
+  "https://elraft-fashion.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+];
 // middleware
 app.use(
   cors({
-    origin: "https://elraft-fashion.vercel.app",
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 // 👇 THIS LINE IS CRITICAL
 app.options("*", cors());
 app.use(express.json());
@@ -32,5 +48,5 @@ mongoose
 module.exports = app; 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port 5000 ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
